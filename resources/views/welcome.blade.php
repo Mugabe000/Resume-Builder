263f0339617134a550fad9ff0d9266f54d452b86
MUGABE Nshuti Ignace <ignacemugabe@gmail.com>
2023-12-04 08:01:58 +0200

    feat: Add navigation link on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resume Builder</title>
 </head>
+
 <body>
-    <livewire:welcome/>
+    <nav>
+        <a href="/login" wire:navigate>Login</a>
+        <a href="/register" wire:navigate>Register</a>
+    </nav>
+    <livewire:welcome />
 </body>
+
 </html>
